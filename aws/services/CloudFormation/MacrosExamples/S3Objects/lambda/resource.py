7cc93733435be930b1363fef4baf0a32eb2c0407
Florian JUDITH <florian.judith.b@gmail.com>
2022-04-25 13:51:15 -0400

    Leverage cfnresponse.py and increased logging

--- aws/services/CloudFormation/MacrosExamples/S3Objects/lambda/resource.py
+++ aws/services/CloudFormation/MacrosExamples/S3Objects/lambda/resource.py
@@ -10,100 +10,137 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-
-from urllib.request import build_opener, HTTPHandler, Request
-import base64
+import logging, json, base64
 import boto3
-import http.client
-import json
+import cfnresponse
 
 s3_client = boto3.client("s3")
 
-
-def sendResponse(event, context, status, message):
-    bucket = event["ResourceProperties"].get("Target", {}).get("Bucket")
-    key = event["ResourceProperties"].get("Target", {}).get("Key")
-
-    body = json.dumps(
-        {
-            "Status": status,
-            "Reason": message,
-            "StackId": event["StackId"],
-            "RequestId": event["RequestId"],
-            "LogicalResourceId": event["LogicalResourceId"],
-            "PhysicalResourceId": f"s3://{bucket}/{key}",
-            "Data": {
-                "Bucket": bucket,
-                "Key": key,
-            },
-        }
-    )
-
-    request = Request(event["ResponseURL"], data=body.encode("utf-8"))
-    request.add_header("Content-Type", "")
-    request.add_header("Content-Length", len(body))
-    request.get_method = lambda: "PUT"
-
-    opener = build_opener(HTTPHandler)
-    response = opener.open(request)
-
+for logger_name in [__name__, 'boto3']:
+    logger = logging.getLogger(logger_name)
+    ch = logging.StreamHandler()
+    logger.setLevel(logging.INFO)
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(logging.Formatter('%(asctime)s - %(threadName)s %(filename)s:%(lineno)d - %(funcName)s() - %(levelname)s - %(message)s'))
+    logger.addHandler(ch)
 
 def handler(event, context):
-    print("Received request:", json.dumps(event, indent=4))
+    logger.info("Received request:\n" + json.dumps(event, indent=4))
 
     request = event["RequestType"]
     properties = event["ResourceProperties"]
-
+    target = properties["Target"]
+    bucket = event['ResourceProperties']['Target']['Bucket']
+    key = event['ResourceProperties']['Target']['Key']
+    
+    target_uri = f's3://{bucket}/{key}'
+    
     if "Target" not in properties or all(
         prop not in properties for prop in ["Body", "Base64Body", "Source"]
     ):
-        return sendResponse(event, context, "FAILED", "Missing required parameters")
+        return cfnresponse.send(event=event, context=context, responseStatus=cfnresponse.FAILED,
+                                responseData={'Bucket': bucket, 'Key': key},
+                                physicalResourceId=target_uri,
+                                reason='Missing required parameters')
 
-    target = properties["Target"]
-
+    # Create or update
     if request in ("Create", "Update"):
+        # Full-text
         if "Body" in properties:
+            logger.info(f'Creating/Updating "{target_uri}" using fulltext body')
             target.update(
                 {
                     "Body": properties["Body"],
                 }
             )
 
-            s3_client.put_object(**target)
+            try:
+                s3_client.put_object(**target)
+            except Exception as e:
+                logger.error(f'Failed to create or update "{target_uri}" from full-text body\n{e}')
+                
+                return cfnresponse.send(event=event, context=context, responseStatus=cfnresponse.FAILED,
+                                        responseData={'Bucket': bucket, 'Key': key},
+                                        physicalResourceId=target_uri,
+                                        reason=f'Failed to create or update from full-text body\n{e}')
 
+        # Base64 encoded
         elif "Base64Body" in properties:
+            logger.info(f'Creating "{target_uri}" using base64 encoded body')
+            
             try:
                 body = base64.b64decode(properties["Base64Body"])
             except:
-                return sendResponse(event, context, "FAILED", "Malformed Base64Body")
+                return cfnresponse.send(event=event, context=context, responseStatus=cfnresponse.FAILED,
+                                        responseData={'Bucket': bucket, 'Key': key},
+                                        physicalResourceId=target_uri,
+                                        reason='Malformed Base64Body')
+            
+            try:
+                target.update({"Body": body})
 
-            target.update({"Body": body})
-
-            s3_client.put_object(**target)
-
+                s3_client.put_object(**target)
+            except Exception as e:
+                logger.error(f'Failed to create or update "{target_uri}" from base64 encoded body\n{e}')
+                
+                return cfnresponse.send(event=event, context=context, responseStatus=cfnresponse.FAILED,
+                                        responseData={'Bucket': bucket, 'Key': key},
+                                        physicalResourceId=target_uri,
+                                        reason=f'Failed to create or update from base64 body\n{e}')
+        # Copy  
         elif "Source" in properties:
             source = properties["Source"]
+            source_uri = f's3://{source["Bucket"]}/{source["Key"]}'
+            logger.info(f'Copying "{source_uri}" to "{target_uri}"')
 
-            s3_client.copy_object(
-                CopySource=source,
-                Bucket=target["Bucket"],
-                Key=target["Key"],
-                MetadataDirective="COPY",
-                TaggingDirective="COPY",
-                ACL=target["ACL"],
-            )
-
+            try:
+                s3_client.copy_object(CopySource=source,
+                                      Bucket=target["Bucket"],
+                                      Key=target["Key"],
+                                      MetadataDirective="COPY",
+                                      TaggingDirective="COPY",
+                                      ACL=target["ACL"])
+            except Exception as e:
+                logger.error(f'Failed to copy {source_uri} to "{target_uri}"\n{e}')
+                
+                return cfnresponse.send(event=event, context=context, responseStatus=cfnresponse.FAILED,
+                                        responseData={'Bucket': bucket, 'Key': key},
+                                        physicalResourceId=target_uri,
+                                        reason=f'Failed to create or update from copy of "{source_uri}"\n{e}')
+                
         else:
-            return sendResponse(event, context, "FAILED", "Malformed body")
+            return cfnresponse.send(event=event, context=context, responseStatus=cfnresponse.FAILED,
+                                    responseData={'Bucket': bucket, 'Key': key},
+                                    physicalResourceId=target_uri,
+                                    reason='Malformed body')
 
-        return sendResponse(event, context, "SUCCESS", "Created")
+        return cfnresponse.send(event=event, context=context, responseStatus=cfnresponse.SUCCESS,
+                                responseData={'Bucket': bucket, 'Key': key},
+                                physicalResourceId=target_uri,
+                                reason=f'Created "{target_uri}"')
 
+    # Delete
     if request == "Delete":
-        s3_client.delete_object(
-            Bucket=target["Bucket"],
-            Key=target["Key"],
-        )
+        logger.info(f'Deleting "{target_uri}"')
+        
+        try:
+            s3_client.delete_object(Bucket=target["Bucket"],
+                                    Key=target["Key"])
+        except Exception as e:
+                logger.error(f'Failed to delete "{target_uri}"\n{e}')
+                
+                return cfnresponse.send(event=event, context=context, responseStatus=cfnresponse.FAILED,
+                                        responseData={'Bucket': bucket, 'Key': key},
+                                        physicalResourceId=target_uri,
+                                        reason=f'Failed to delete object\n{e}')
 
-        return sendResponse(event, context, "SUCCESS", "Deleted")
-
-    return sendResponse(event, context, "FAILED", f"Unexpected: {request}")
+        return cfnresponse.send(event=event, context=context, responseStatus=cfnresponse.SUCCESS,
+                            responseData={'Bucket': bucket, 'Key': key},
+                            physicalResourceId=target_uri,
+                            reason=f'Deleted "{target_uri}"')
+        
+        
+    return cfnresponse.send(event=event, context=context, responseStatus=cfnresponse.FAILED,
+                            responseData={'Bucket': bucket, 'Key': key},
+                            physicalResourceId=target_uri,
+                            reason=f'Unexpected {request}')
